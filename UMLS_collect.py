d122197eed510788ac10712695211d0d98becbc3
x-wei <wistar.sjtu@gmail.com>
2016-11-03 09:13:09 +0100

    test without last other label

--- UMLS_collect.py
+++ UMLS_collect.py
@@ -30,9 +30,11 @@ each .pk file contains a list, each element in list corresponds to a noteevent
 each noteevent is represented as a list of lists of dicts (one list per sentence?), 
 the concepts are stored in the dicts
 '''
+import resource
+resource.setrlimit(resource.RLIMIT_NOFILE, (2048,4096))# IOError: [Errno 24] Too many open files
 
 print 'start extracting concepts...'
-for fname in tqdm(os.listdir(NOTE_DATA_DIR)[:]):
+for fname in tqdm(os.listdir(NOTE_DATA_DIR)[4931+645+6831+4811+790+90:]):
     fpath = os.path.join(NOTE_DATA_DIR, fname)
     df = pd.read_csv(fpath)
     umls = []
@@ -41,7 +43,8 @@ for fname in tqdm(os.listdir(NOTE_DATA_DIR)[:]):
         umls.append(res)
     fout_name = fname[:-4]+'.pk' # output name: sid.pk
     fout_path = os.path.join(UMLS_DATA_DIR, fout_name)
-    with open(fout_path, 'wb') as f: 
+    with open(fout_path, 'wb') as f:
         pk.dump(umls, f, pk.HIGHEST_PROTOCOL)
+        
 print('done!')
 
