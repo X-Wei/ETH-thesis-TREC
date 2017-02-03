1506ec842d9747586f5948fecba06df1034a57e8
x-wei <wistar.sjtu@gmail.com>
2017-02-03 15:39:54 +0100

    re-organize code

---
+++ 2-DRMM/data_prep.py
@@ -0,0 +1,36 @@
+from tqdm import tqdm
+from lxml import etree
+import os, nltk, sys, time
+import numpy as np
+from numpy.linalg import norm
+import pandas as pd
+import cPickle as pk
+
+from settings import * 
+np.random.seed(1) 
+
+topic_tree = etree.parse('topics2016.xml')
+
+def get_topic(i):# returns the summary string of the ith topic
+    summary = topic_tree.xpath('//topic[@number="%d"]/summary/text()'%i)[0]
+    return str(summary).lower()
+
+pmcid2fpath = {}
+for subdir1 in os.listdir(PMC_PATH):
+    for subdir2 in os.listdir(os.path.join(PMC_PATH, subdir1)):
+        diry = os.path.join(PMC_PATH, subdir1, subdir2)
+        for fn in os.listdir(diry):
+            pmcid = fn[:-5]
+            fpath = os.path.join(diry, fn)
+            pmcid2fpath[pmcid] = fpath
+
+def get_article_abstract(pmcid):
+    fpath = pmcid2fpath[pmcid]
+    tree = etree.parse(fpath)
+    ret = u'' + tree.xpath('string(//article-title)') + '\n'
+    abstracts = tree.xpath('//abstract')
+#     abstracts = tree.xpath('//p')
+    ret += u' '.join( [abstract.xpath('string(.)') for abstract in abstracts] )
+    if len(ret.split())<20: 
+        raise Exception(u'abstraction too short: '+pmcid + ret)
+    return ret.lower()
---
+++ 2-DRMM/settings.py
@@ -0,0 +1,4 @@
+W2V_FPATH = '/local/XW/DATA/WORD_EMBEDDINGS/biomed-w2v-200.txt'
+GLOVE_FPATH = '/local/XW/DATA/WORD_EMBEDDINGS/glove.6B.200d.txt'
+PMC_PATH = '/local/XW/DATA/TREC/PMCs/' # path that stores all xml files of the docset
+WD_PLACEHOLDER = '</s>'
