232f6b54dbb9f6383ffea0d415769df4bad038d2
x-wei <wistar.sjtu@gmail.com>
2017-02-18 14:11:17 +0100

    run embedding on R52 dataset (for IR test on tipster)

---
+++ 1-CNN-embedding/r52_data_prep.py
@@ -0,0 +1,76 @@
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from settings import * 
+np.random.seed(1) 
+
+training_set, testing_set = [], [] # list of (text, label) pairs
+
+with open(R52_FPATH_TRAIN) as f:
+    for line in f: 
+        label, text = line.split('\t', 1)
+        training_set.append( (label, text) )
+
+with open(R52_FPATH_TEST) as f:
+    for line in f: 
+        label, text = line.split('\t', 1)
+        testing_set.append( (label, text) )
+
+all_labels = set(zip(*training_set)[0]) # 52 unique labels
+N_LABELS = len(all_labels)
+label2id = {} # mapping label(string) to an idx
+for i, label in enumerate( sorted(all_labels) ):
+    label2id[label] = i 
+
+
+# tokenize texts, pad to sequences of ids
+tokenizer = Tokenizer(nb_words=MAX_NB_WORDS)
+texts = zip(*training_set)[1]
+tokenizer.fit_on_texts(texts)
+word2idx = tokenizer.word_index # dictionary mapping words (str) ---> their index (int)
+
+def getXY(dataset):
+    labels, texts = zip(*dataset)
+    seqs = tokenizer.texts_to_sequences(texts) # turn article into seq of ids
+    seqs_padded = pad_sequences(seqs, maxlen=MAX_SEQ_LEN) # turn into fix-length sequences
+    X = np.array(seqs_padded)
+    Y = []
+    for label in labels: 
+        _onehot = np.zeros(N_LABELS)
+        k = label2id[label]
+        _onehot[k] = 1
+        Y.append(_onehot)
+    Y = np.array(Y)
+    return X, Y
+X_train, Y_train = getXY(training_set)
+X_val, Y_val     = getXY(testing_set)
+
+
+# prepare embeddings 
+nb_words = min(MAX_NB_WORDS, len(word2idx))
+glove = {} # maps word ---> embedding vector
+with open(GLOVE_FPATH) as f:
+    for line in tqdm(f, total=400000):
+        vals = line.split()
+        word = vals[0]
+        if word in word2idx or word=='</s>':
+            glove[word] = np.asarray(vals[1:], dtype='float')
+print 'found %d word vectors.' % len(glove)
+embedding_glove = np.zeros( (nb_words+1, EMBEDDING_DIM) ) # +1 because ids in sequences starts from 1 ?
+for word,wd_id in word2idx.items(): 
+    if wd_id > MAX_NB_WORDS or word not in glove: # there might be 0 rows in embedding matrix
+        continue # word_id>MAX_NB_WORDS, this id is not in the generated sequences, discard
+    embedding_glove[wd_id,:] = glove[word]
+
+# output
+data_to_pickle = {
+    'embedding_glove' : embedding_glove,
+    'X_train'         : X_train,
+    'Y_train'         : Y_train,
+    'X_val'           : X_val,
+    'Y_val'           : Y_val,
+}
+
+with open(R52_PK_FPATH, 'wb') as fout:
+    pk.dump(data_to_pickle, fout, pk.HIGHEST_PROTOCOL)
+print 'processed data is written into %s' % R52_PK_FPATH
--- 1-CNN-embedding/settings.py
+++ 1-CNN-embedding/settings.py
@@ -9,9 +9,10 @@ import cPickle as pk
 # constants
 N_LABELS      = 50 # predict only on top K frequent icd codes
 N_SIDHID      = 58328  # number of unique (sid,hid) pairs
-MAX_SEQ_LEN   = 1000 # max length of input sequence (pad/truncate to fixed length)
+MAX_SEQ_LEN   = 500 # max length of input sequence (pad/truncate to fixed length)
 MAX_NB_WORDS  = 100000 # top 100k most freq words
 EMBEDDING_DIM = 200
+DATE_TODAY    = time.strftime('%m%d')
 
 # for data preparation
 NOTES_DIR   = '/local/XW/DATA/MIMIC/noteevents_by_sid_hid/'
@@ -19,7 +20,10 @@ ICD_FPATH   = '../data/sid_hid_diagicds.txt'
 PK_FPATH    = '../data/CNN_embedding_preprocessed.pk'
 W2V_FPATH   = '/local/XW/DATA/WORD_EMBEDDINGS/biomed-w2v-200.txt'
 GLOVE_FPATH = '/local/XW/DATA/WORD_EMBEDDINGS/glove.6B.200d.txt'
-
+# for tipster dataset (train using R52 dataset)
+R52_FPATH_TRAIN = '/local/XW/DATA/phd-datasets/r52-train-all-terms.txt'
+R52_FPATH_TEST  = '/local/XW/DATA/phd-datasets/r52-test-all-terms.txt'
+R52_PK_FPATH    = '../data/R52_CNN_embedding_preprocessed.pk'
 
 # for model training
 MODEL_PATH       = '../models/'
